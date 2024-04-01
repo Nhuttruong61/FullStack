cba6bc25d5d01302bda9b177f9f5f880f3df7ea9
Nhuttruong61 <Nhuttruong2261@gmail.com>
2024-04-01 14:58:58 +0700

    deloy 8

--- client/src/componets/sidebar/Sidebar.jsx
+++ client/src/componets/sidebar/Sidebar.jsx
@@ -4,14 +4,14 @@ import { IoMdClose } from "react-icons/io";
 
 function Sidebar({ children, setListMenuRp }) {
   return (
-    <div className="sidebar" onClick={() => setListMenuRp(false)}>
-      <div className="sidebar--list" onClick={(e) => e.stopPropagation()}>
-        <div className="sidebar--list--close">
+    <div className="sidebarMN" onClick={() => setListMenuRp(false)}>
+      <div className="sidebarMN--list" onClick={(e) => e.stopPropagation()}>
+        <div className="sidebarMN--list--close">
           <span onClick={() => setListMenuRp(false)}>
             <IoMdClose size={24} />
           </span>
         </div>
-        <div className="sidebar--list--children">{children}</div>
+        <div className="sidebarMN--list--children">{children}</div>
       </div>
     </div>
   );
